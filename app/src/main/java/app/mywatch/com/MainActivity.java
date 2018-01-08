1512802abdf120e933bb5997734ad39a7e7cde85
DragosBarbu <dragosbarbu2011@gmail.com>
2018-01-08 18:42:11 +0100

    added notificationSenderFactory and made requestPermission work even after it gets canceled

--- app/src/main/java/app/mywatch/com/MainActivity.java
+++ app/src/main/java/app/mywatch/com/MainActivity.java
@@ -1,13 +1,17 @@
 package app.mywatch.com;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -21,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
+    private static final int PERMISSION_REQUESTCODE = 142;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,20 +34,21 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkPermission();
-                new CalendarEvent().send("AAAAAA", MainActivity.this);
+                new NotificationSenderFactory().createInstance(NotificationSenderFactory.NotificationType.CALENDAR)
+                        .send("AAAAAA", MainActivity.this);
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
-        if (savedInstanceState == null)
+        if (savedInstanceState == null) {
+            requestPermission();
             getSupportFragmentManager().beginTransaction().replace(R.id.container, MyPreferenceFragment.newInstance()).commit();
-
+        }
     }
 
     @Override
@@ -53,43 +60,43 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        if (id == R.id.action_settings) return true;
         return super.onOptionsItemSelected(item);
     }
 
-    private void checkPermission() {
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_CALENDAR)
-                != PackageManager.PERMISSION_GRANTED) {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUESTCODE) {
+            for (int i = 0; i < permissions.length; i++) {
+                String permission = permissions[i];
+                int grantResult = grantResults[i];
+                if (permission.equals(Manifest.permission.WRITE_CALENDAR) && grantResult != PackageManager.PERMISSION_GRANTED)
+                    requestPermission();
+            }
+        }
+    }
 
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
             // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.WRITE_CALENDAR)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_CALENDAR)) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setMessage(R.string.permission_explanation)
+                        .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.WRITE_CALENDAR},
+                                        PERMISSION_REQUESTCODE);
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, null).show();
             } else {
-
                 // No explanation needed, we can request the permission.
-
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.WRITE_CALENDAR},
-                        74);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
+                        PERMISSION_REQUESTCODE);
             }
         }
     }
---
+++ app/src/main/java/app/mywatch/com/NotificationSenderFactory.java
@@ -0,0 +1,23 @@
+package app.mywatch.com;
+
+/**
+ * Created by dragos on 1/8/18.
+ */
+
+public class NotificationSenderFactory {
+
+    public enum NotificationType {
+        CALENDAR,
+        SMS
+    }
+
+    public NotificationSender createInstance(NotificationType type) {
+        NotificationSender sender;
+        if (type == NotificationType.CALENDAR)
+            sender = new CalendarEvent();
+        else
+            throw new NullPointerException("SMS not yet supported");
+
+        return sender;
+    }
+}
